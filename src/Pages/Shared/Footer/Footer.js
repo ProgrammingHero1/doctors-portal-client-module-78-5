fb75e5b781189039fc67be1339788b3aa4ce27b7
Programming Hero <programming.hero1@gmail.com>
2022-11-10 06:44:37 -0600

    added footer

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,10 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import footer from '../../../assets/images/footer.png';
 
 const Footer = () => {
     return (
-        <div>
-            <h2>This is Footer</h2>
-        </div>
+        <footer
+            style={{
+                background: `url(${footer})`,
+                backgroundSize: 'cover'
+            }}
+        className="p-10">
+            <div className='footer'>
+                <div>
+                    <span className="footer-title">Services</span>
+                    <Link to="/" className="link link-hover">Branding</Link>
+                    <Link to="/" className="link link-hover">Design</Link>
+                    <Link to="/" className="link link-hover">Marketing</Link>
+                    <Link to="/" className="link link-hover">Advertisement</Link>
+                </div>
+                <div>
+                    <span className="footer-title">Company</span>
+                    <Link to="/" className="link link-hover">About us</Link>
+                    <Link to="/" className="link link-hover">Contact</Link>
+                    <Link to="/" className="link link-hover">Jobs</Link>
+                    <Link to="/" className="link link-hover">Press kit</Link>
+                </div>
+                <div>
+                    <span className="footer-title">Legal</span>
+                    <Link to="/" className="link link-hover">Terms of use</Link>
+                    <Link to="/" className="link link-hover">Privacy policy</Link>
+                    <Link to="/" className="link link-hover">Cookie policy</Link>
+                </div>
+            </div>
+            <div className='text-center mt-32'>
+                <p>Copyright © 2022 - All right reserved by ACME Industries Ltd</p>
+            </div>
+        </footer>
     );
 };
 
