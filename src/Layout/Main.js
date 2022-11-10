81c56298b0df3c10b22c0bf4f124ab1ed861c365
Programming Hero <programming.hero1@gmail.com>
2022-11-09 20:57:24 -0600

    initial project setup with router setup

---
+++ src/Layout/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Pages/Shared/Footer/Footer';
+import Navbar from '../Pages/Shared/Navbar/Navbar';
+
+const Main = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../Layout/Main";
+import Home from "../../Pages/Home/Home/Home";
+import Login from "../../Pages/Login/Login";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>, 
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            }
+        ]
+    }
+])
+
+export default router;
