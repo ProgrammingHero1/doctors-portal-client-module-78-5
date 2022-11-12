b0374b6c8f3df90284bf5cb28f93c9b2348e92a1
Programming Hero <programming.hero1@gmail.com>
2022-11-11 22:46:05 -0600

    install react hook form

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 const Login = () => {
+    const { register, handleSubmit } = useForm()
+    const [data, setData] = useState('');
+
     return (
-        <div>
-            <h2>This is Login</h2>
+        <div className='h-[800px] flex justify-center items-center'>
+            <div>
+                <h2 className='text-4xl'>Login</h2>
+                <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Email</span></label>
+                        <input type="text" {...register("email")} className="input input-bordered w-full max-w-xs"/>
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Email</span></label>
+                        <input type="password" {...register("password")} className="input input-bordered w-full max-w-xs"/>
+                    </div>
+                    
+                    <p>{data}</p>
+                    <input type="submit" />
+                </form>
+            </div>
         </div>
     );
 };
