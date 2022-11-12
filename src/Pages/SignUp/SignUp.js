81b61266a3f3b7d3d5d858aa31b76116747ab894
Programming Hero <programming.hero1@gmail.com>
2022-11-12 14:36:31 -0600

    react hook form regex validation

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -34,7 +34,8 @@ const SignUp = () => {
                         <label className="label"> <span className="label-text">Password</span></label>
                         <input type="password" {...register("password", {
                             required: "Password is required",
-                            minLength: {value: 6, message: "Password must be 6 characters long"}
+                            minLength: {value: 6, message: "Password must be 6 characters long"},
+                            pattern: {value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/, message: 'Password must be strong'}
                         })} className="input input-bordered w-full max-w-xs" />
                         {errors.password && <p className='text-red-500'>{errors.password.message}</p>}
                     </div>
