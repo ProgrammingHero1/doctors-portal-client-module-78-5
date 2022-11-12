acba6492f1e8a46d234618f32884be8ddfc85724
Programming Hero <programming.hero1@gmail.com>
2022-11-12 13:54:38 -0600

    handle error using google form

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 
@@ -7,7 +7,7 @@ const Login = () => {
 
     const handleLogin = data => {
         console.log(data);
-    }
+    }   
 
     return (
         <div className='h-[800px] flex justify-center items-center'>
@@ -21,16 +21,18 @@ const Login = () => {
                                 required: "Email Address is required" 
                             })}
                             className="input input-bordered w-full max-w-xs" />
-                            {errors.email && <p role="alert">{errors.email?.message}asdfasdfasdf</p>}
+                            {errors.email && <p className='text-red-600'>{errors.email?.message}</p>}
                     </div>
                     <div className="form-control w-full max-w-xs">
                         <label className="label"> <span className="label-text">Password</span></label>
                         <input type="password"
                             {...register("password", {
-                                required: true
+                                required: "Password is required",
+                                minLength: { value: 6, message: 'Password must be 6 characters or longer'}
                             })}
                             className="input input-bordered w-full max-w-xs" />
                         <label className="label"> <span className="label-text">Forget Password?</span></label>
+                        {errors.password && <p className='text-red-600'>{errors.password?.message}</p>}
                     </div>
                     <input className='btn btn-accent w-full' value="Login" type="submit" />
                 </form>
