ca4abe80dd454efb86ea99a464865ec9d48c4823
Programming Hero <programming.hero1@gmail.com>
2022-11-11 23:28:25 -0600

    basic login page

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,27 +1,34 @@
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     const { register, handleSubmit } = useForm()
-    const [data, setData] = useState('');
+
+    const handleLogin = data => {
+        console.log(data);
+    }
 
     return (
         <div className='h-[800px] flex justify-center items-center'>
-            <div>
-                <h2 className='text-4xl'>Login</h2>
-                <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+            <div className='w-96 p-7'>
+                <h2 className='text-xl text-center'>Login</h2>
+                <form onSubmit={handleSubmit(handleLogin)}>
                     <div className="form-control w-full max-w-xs">
                         <label className="label"> <span className="label-text">Email</span></label>
-                        <input type="text" {...register("email")} className="input input-bordered w-full max-w-xs"/>
+                        <input type="text" {...register("email")} className="input input-bordered w-full max-w-xs" />
                     </div>
                     <div className="form-control w-full max-w-xs">
-                        <label className="label"> <span className="label-text">Email</span></label>
-                        <input type="password" {...register("password")} className="input input-bordered w-full max-w-xs"/>
+                        <label className="label"> <span className="label-text">Password</span></label>
+                        <input type="password" {...register("password")} className="input input-bordered w-full max-w-xs" />
+                        <label className="label"> <span className="label-text">Forget Password?</span></label>
                     </div>
-                    
-                    <p>{data}</p>
-                    <input type="submit" />
+                    <input className='btn btn-accent w-full' value="Login" type="submit" />
                 </form>
+                <p>New to Doctors Portal <Link className='text-secondary' to="/signup">Create new Account</Link></p>
+                <div className="divider">OR</div>
+                <button className='btn btn-outline w-full'>CONTINUE WITH GOOGLE</button>
+
             </div>
         </div>
     );
