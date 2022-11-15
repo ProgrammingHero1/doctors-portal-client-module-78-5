7e8746b30068e7a271d41322844355b646f130d7
Programming Hero <programming.hero1@gmail.com>
2022-11-14 22:57:33 -0600

    new version of the api

--- src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -11,7 +11,7 @@ const AvailableAppointments = ({ selectedDate }) => {
     const { data: appointmentOptions = [], refetch, isLoading } = useQuery({
         queryKey: ['appointmentOptions', date],
         queryFn: async () => {
-            const res = await fetch(`http://localhost:5000/appointmentOptions?date=${date}`);
+            const res = await fetch(`http://localhost:5000/v2/appointmentOptions?date=${date}`);
             const data = await res.json();
             return data
         }
