369e3464f2743e6082cbad36515bf6cc56cccbd4
Programming Hero <programming.hero1@gmail.com>
2022-11-17 17:13:35 -0600

    basic add a doctor form

--- src/Pages/Dashboard/AddDoctor/AddDoctor.js
+++ src/Pages/Dashboard/AddDoctor/AddDoctor.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const AddDoctor = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const handleAddDoctor = data => {
+        console.log(data);
+    }
     return (
-        <div>
+        <div className='w-96 p-7'>
             <h2 className="text-4xl">Add A Doctor</h2>
+            <form onSubmit={handleSubmit(handleAddDoctor)}>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label"> <span className="label-text">Name</span></label>
+                    <input type="text" {...register("name", {
+                        required: "Name is Required"
+                    })} className="input input-bordered w-full max-w-xs" />
+                    {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label"> <span className="label-text">Email</span></label>
+                    <input type="email" {...register("email", {
+                        required: true
+                    })} className="input input-bordered w-full max-w-xs" />
+                    {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label"> <span className="label-text">Specialty</span></label>
+                    <select className="select select-ghost w-full max-w-xs">
+                        <option disabled selected>Pick a Specialty</option>
+                        <option>Svelte</option>
+                        <option>Vue</option>
+                        <option>React</option>
+                    </select>
+                </div>
+                <input className='btn btn-accent w-full mt-4' value="Add Doctor" type="submit" />
+            </form>
         </div>
     );
 };
