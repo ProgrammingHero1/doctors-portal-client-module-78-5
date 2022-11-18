89cf8785b764d4f9050907ea7744b0212c324b43
Programming Hero <programming.hero1@gmail.com>
2022-11-17 23:01:22 -0600

    display price

--- src/Pages/Appointment/AvailableAppointments/AppointmentOption.js
+++ src/Pages/Appointment/AvailableAppointments/AppointmentOption.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 const AppointmentOption = ({ appointmentOption, setTreatment }) => {
-    const { name, slots } = appointmentOption;
+    const { name, price, slots } = appointmentOption;
     return (
         <div className="card shadow-xl">
             <div className="card-body text-center">
                 <h2 className="text-2xl text-secondary font-bold text-center">{name}</h2>
                 <p>{slots.length > 0 ? slots[0] : 'Try Another day'}</p>
                 <p>{slots.length} {slots.length > 1 ? 'spaces' : 'space'} available</p>
+                <p><small>Price: ${price}</small></p>
                 <div className="card-actions justify-center">
                     <label
                         disabled={slots.length === 0}
