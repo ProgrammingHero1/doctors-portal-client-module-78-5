3d50227445c12af9fe6d3a98f9a49b7ae8ccf97c
Programming Hero <programming.hero1@gmail.com>
2022-11-17 22:06:53 -0600

    drawer background color should not pop up while displaying modal

--- src/Layout/DashboardLayout.js
+++ src/Layout/DashboardLayout.js
@@ -17,7 +17,7 @@ const DashboardLayout = () => {
                 </div>
                 <div className="drawer-side">
                     <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
-                    <ul className="menu p-4 w-80 bg-base-100 text-base-content">
+                    <ul className="menu p-4 w-80 text-base-content">
                         <li><Link to="/dashboard">My Appointments</Link></li>
                         {
                             isAdmin && <>
